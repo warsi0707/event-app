c6c2737b6cea7a52f562197e4170c8cf4e3aa6f9
Samir Warsi <samirwarsi@Samirs-MacBook-Air.local>
2025-11-08 16:44:46 +0530

    redux-toolkit-setup

---
+++ frontend/src/redux/slice/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+
+    }
+})
+
+const userReducer = userSlice.reducer;
+
+export default userReducer;
